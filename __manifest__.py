dd656282055c2057f36210cf4c9f69fb2c44735c
gabi-b612 <20km095@esisalama.org>
2024-12-27 14:38:37 +0200

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'caperpooling application',
+    'version': '1.0.0',
+    'description': """"
+            Module to help you to organize the traject with your employees.
+            Make your life easier
+        """,
+    
+    'author': 'Gabriel',
+    'website' : 'www.exemple.com',
+    'license': 'LGPL-3',
+    'category': 'Customizations',
+    'depends' : ['base', 'sale'],
+    
+}
